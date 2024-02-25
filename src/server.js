416e650d7336274a91b9dba7a5293f5026a562e5
theUndefine12 <161183402+theUndefine12@users.noreply.github.com>
2024-02-25 09:47:54 +0500

    Complected

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 import 'colors'
 import fileRoutes from './Multer/multer.routes.js'
 import {fileURLToPath} from 'url'
 import path from 'path'
 
-dotenv.config()
-
 
 const app = express()
-const port = process.env.PORT || 1111
-const mongoUrl = process.env.URL 
+const port = 3000
+const mongoUrl = "" 
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
